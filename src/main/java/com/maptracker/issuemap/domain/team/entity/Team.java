bd189bd062a10a928d9ae64103fcbda5bb6859fb
ijnim1121 <jimin3347@naver.com>
2025-01-15 16:55:48 +0900

    feat: Builder 추가

--- src/main/java/com/maptracker/issuemap/domain/team/entity/Team.java
+++ src/main/java/com/maptracker/issuemap/domain/team/entity/Team.java
@@ -1,11 +1,15 @@
 package com.maptracker.issuemap.domain.team.entity;
 import com.maptracker.issuemap.domain.project.entity.Project;
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 import java.util.List;
 
 @Entity
 @Getter
+@NoArgsConstructor
 public class Team {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -17,4 +21,9 @@ public class Team {
 
     @OneToMany(mappedBy = "team", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Project> projects;
+
+    @Builder
+    public Team(String teamName) {
+        this.teamName = teamName;
+    }
 }
