28c4cb7b1be7fcc9adb2004aeebf681ff00ac41b
ijnim1121 <jimin3347@naver.com>
2025-01-16 14:18:41 +0900

    fix: swagger 오류 수정

--- src/main/java/com/maptracker/issuemap/common/config/SecurityConfig.java
+++ src/main/java/com/maptracker/issuemap/common/config/SecurityConfig.java
@@ -19,14 +19,22 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(AbstractHttpConfigurer::disable) // CSRF 비활성화
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll() // Swagger 관련 경로 허용
-                        .requestMatchers("/api/teams/**").permitAll() // 특정 API 경로 허용
-                        .requestMatchers("/api/users/signup").permitAll() // 회원가입 경로 허용
-                        .anyRequest().authenticated() // 그 외 요청 인증 필요
+                        // Swagger 관련 경로 허용
+                        .requestMatchers(
+                                "/swagger-ui/**",
+                                "/swagger-ui.html",
+                                "/v3/api-docs/**",
+                                "/swagger-resources/**",
+                                "/webjars/**"
+                        ).permitAll()
+                        // 특정 API 경로 허용
+                        .requestMatchers("/api/teams/**").permitAll()
+                        .requestMatchers("/api/users/signup").permitAll()
+                        .anyRequest().authenticated()
                 )
-                .httpBasic(httpBasic -> httpBasic.realmName("YourRealm")); // HTTP Basic 인증 활성화 및 Realm 설정
+                .httpBasic(AbstractHttpConfigurer::disable);
 
         return http.build();
     }
